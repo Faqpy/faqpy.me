c6520f60a0fb612200c04cd92e1828c0092cf3b5
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-05-30 17:57:17 +0530

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,10 @@
     <p style="font-size: 20px; font-style: normal;">(a.k.a witchie)</p>
     <p>Heya, I am faqpy! <CustomEmoji src={miauWhat} name=":miauWhat:"/> <CustomEmoji src={fw} name=":fw:"/></p>
     <p>I'm a self-taught web developer (currently learning).</p>
-    <a href="https://bsky.app/profile/faqpy.me" target="_blank"><button class="social-btns">BSky</button></a>
-    <a href="mailto:faqpy@proton.me" target="_blank"><button class="social-btns">Mail</button></a>
-    <a href="https://discord.com/users/277867160796397568" target="_blank"><button class="social-btns">Discord</button></a>
-    <a href={resolve("/contact")}><button class="social-btns">Contact</button></a>
+    <a href="https://bsky.app/profile/faqpy.me" target="_blank" title="Rarely used."><button class="social-btns">BSky</button></a>
+    <a href="mailto:faqpy@proton.me" target="_blank" title="Choose b/w Mail and Contact according to your suitability."><button class="social-btns">Mail</button></a>
+    <a href="https://discord.com/users/277867160796397568" target="_blank" title="Discord is often preferred for faster replies."><button class="social-btns">Discord</button></a>
+    <a href={resolve("/contact")} title="Choose b/w Mail and Contact according to your suitability."><button class="social-btns">Contact</button></a>
 </main>
 <footer>
     <p>i cnat cod &nbsp;•&nbsp; witchie</p>
