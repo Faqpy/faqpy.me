4f19b1fa4933f71bfd81be5b3fc8c9a5ab57aca9
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-05-31 23:00:44 +0530

    adding a youtube-music profile and adjusting the order of profiles by name

--- src/routes/profiles/+page.svelte
+++ src/routes/profiles/+page.svelte
@@ -1,25 +1,20 @@
 <main>
     <h1><span><a class="title-span" href={resolve("/")}>witchie</a></span> です!</h1>
-    
-    <h3>Game Profiles</h3>
     <div class="profile-button-container">
         <ul class="buttons">
             <li>
                 <a href="https://github.com/faqpy/faqpy.me" target="_blank" rel="noopener noreferrer">
                     <button id="src_btn" title="Source code of this page">
-                        <Icon icon="simple-icons:github" class="btn-icon" />
+                        <Icon icon="line-md:github-loop" class="btn-icon" />
                         <h3 translate="yes">source code</h3>
                     </button>
                 </a>
             </li>
-            <li>
-                <a href="https://steamcommunity.com/id/schizofuck/" target="_blank" rel="noopener noreferrer">
-                    <button title="my Steam profile">
-                        <Icon icon="simple-icons:steam" class="btn-icon" />
-                        <h3 translate="no">steam profile</h3>
-                    </button>
-                </a>
-            </li>            
+        </ul>
+    </div>
+    <h3>Game Profiles</h3>
+    <div class="profile-button-container">
+        <ul class="buttons">
             <li>
                 <a href="https://gdbrowser.com/u/faqpy" target="_blank" rel="noopener noreferrer">
                     <button title="my GD profile">
@@ -52,35 +47,19 @@
                     </button>
                 </a>
             </li>
+            <li>
+                <a href="https://steamcommunity.com/id/schizofuck/" target="_blank" rel="noopener noreferrer">
+                    <button title="my Steam profile">
+                        <Icon icon="simple-icons:steam" class="btn-icon" />
+                        <h3 translate="no">steam profile</h3>
+                    </button>
+                </a>
+            </li>            
         </ul>
     </div>
     <h3>Social Profiles</h3>
     <div class="profile-button-container">
         <ul class="buttons">
-            <li>
-                <a href="https://github.com/faqpy/" target="_blank" rel="noopener noreferrer">
-                    <button id="src_btn" title="Github profile">
-                        <Icon icon="simple-icons:github" class="btn-icon" />
-                        <h3 translate="yes">github</h3>
-                    </button>
-                </a>
-            </li>
-            <li>
-                <a href="https://www.x.com/faqpy/" target="_blank" rel="noopener noreferrer">
-                    <button title="Twitter profile">
-                        <Icon icon="simple-icons:twitter" class="btn-icon" />
-                        <h3 translate="no">twitter</h3>
-                    </button>
-                </a>
-            </li>
-            <li>
-                <a href="https://www.instagram.com/faqpy/" target="_blank" rel="noopener noreferrer">
-                    <button title="Instagram profile">
-                        <Icon icon="simple-icons:instagram" class="btn-icon" />
-                        <h3 translate="no">instagram</h3>
-                    </button>
-                </a>
-            </li>
             <li>
                 <a href="https://bsky.app/profile/faqpy.me" target="_blank" rel="noopener noreferrer">
                     <button title="Bluesky profile">
@@ -94,6 +73,38 @@
                     <button title="@witchie">
                         <Icon icon="simple-icons:discord" class="btn-icon" />
                         <h3 translate="no">discord</h3>
+                    </button>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/faqpy/" target="_blank" rel="noopener noreferrer">
+                    <button id="src_btn" title="Github profile">
+                        <Icon icon="simple-icons:github" class="btn-icon" />
+                        <h3 translate="yes">github</h3>
+                    </button>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/faqpy/" target="_blank" rel="noopener noreferrer">
+                    <button title="Instagram profile">
+                        <Icon icon="simple-icons:instagram" class="btn-icon" />
+                        <h3 translate="no">instagram</h3>
+                    </button>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.x.com/faqpy/" target="_blank" rel="noopener noreferrer">
+                    <button title="Twitter profile">
+                        <Icon icon="simple-icons:twitter" class="btn-icon" />
+                        <h3 translate="no">twitter</h3>
+                    </button>
+                </a>
+            </li>
+            <li>
+                <a href="https://music.youtube.com/@Schizofuck" target="_blank" rel="noopener noreferrer">
+                    <button title="YouTube Music">
+                        <Icon icon="simple-icons:youtubemusic" class="btn-icon" />
+                        <h3 translate="no">youtube music</h3>
                     </button>
                 </a>
             </li>
